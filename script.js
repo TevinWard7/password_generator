97b7a04c699709b3ac0b8e8d43adbf6b4e339458
Tevin <tevinward7@gmail.com>
2020-03-06 00:25:48 -0500

    commented out lines 1-8 and added code for promt

--- script.js
+++ script.js
@@ -3,12 +3,17 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  //var password = generatePassword();
+  //var passwordText = document.querySelector("#password");
+  //passwordText.value = password;
 
-  passwordText.value = password;
-
+  //my code
+  var passwordLength = prompt("How long do you want your password?")
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+
