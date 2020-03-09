7fb9b015a4afc26810a24686447ce37d3b38fc70
Tevin <tevinward7@gmail.com>
2020-03-09 00:21:35 -0400

    changed prompt to confirms for all questions after the first

--- script.js
+++ script.js
@@ -1,39 +1,36 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+//Variables
+var length;
+var lowerCase;
+var upperCase;
+var numeric;
+var specialCharacters;
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  passwordText.value = password;
+	var password = generatePassword();
+	var passwordText = document.querySelector("#password");
+	passwordText.value = password;
 }
 
 function passwordSelection() {
+	//Question 1
+	length = prompt("How many characters? (enter number)");
+	if (length > 8 && length < 128) {
+		//Question 2
+		lowerCase = confirm("Want lower case characters?");
+		//Question 3
+		upperCase = confirm("Want uppercase characters?");
+		//Question 4
+		numeric = confirm("Want numbers?");
+		//Question 5
+		specialCharacters = confirm("Want special characters?");
+	}
 
-  //Variables
-  var length = prompt("How many characters? (enter number)");
-  var lowerCase;
-  var upperCase;
-  var numeric;
-  var specialCharacters;
-
-
-  //Conditions 
-  if (length > 8 && length < 128) {
-    lowerCase = prompt("Want lower case characters?")
-
-    if (lowerCase === "yes" || lowerCase === "no") {
-      upperCase = prompt("Want uppercase characters?")
-    }
-
-    else {
-      alert("Must enter yes or no")
-    }
-  }
-
-  else {
-    alert("Must enter number 8-128")
-  }
+	else {
+		alert("Must enter number 8-128");
+	}
 }
 
 // Add event listener to generate button
